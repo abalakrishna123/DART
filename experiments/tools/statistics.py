added covariate shift logging + fixed bias-variance logging

experiments/tools/statistics.py
@@ -21,7 +21,7 @@ def eval_agent_statistics_cont(env, agent, sup, T, num_samples=1):
     return stats(losses)
 
 
-def evaluate_bias_variance(env, agent, sup, T, num_samples=1):
+def evaluate_bias_variance_cont(env, agent, sup, dist_gen, T, num_samples=1):
     s = env.reset()
     biases = []
     variances = []
@@ -37,7 +37,9 @@ def evaluate_bias_variance(env, agent, sup, T, num_samples=1):
             ensemble_idx = np.random.randint(len(a_ensemble_list))
             a_ensemble = a_ensemble_list[ensemble_idx] # \pi^D_\theta(s)
 
-            next_s, r, done, _ = env.step(a)
+            # Need to evaluate bias/variance on distribution generating parameter
+            a_dist_gen = dist_gen.sample_action(s)
+            next_s, r, done, _ = env.step(a_dist_gen) 
             s = next_s
             bias += (a - a_sup)**2
             variance += (a - a_ensemble)**2
@@ -52,6 +54,51 @@ def evaluate_bias_variance(env, agent, sup, T, num_samples=1):
         variances.append(variance)
             
     return biases, variances
+
+
+def evaluate_covariate_shift_cont(env, agent, sup, dist_gen, T, num_samples=1):
+    s = env.reset()
+    learned_dist_losses = []
+    dist_gen_param_losses = []
+
+    for i in range(num_samples):
+        learner_loss, dist_gen_loss = 0, 0
+
+        t = 0
+        while t < T:
+            a = agent.sample_action(s)
+            a_sup = sup.intended_action(s)
+            # Evaluate losses on learner's distribution
+            next_s, r, done, _ = env.step(a) 
+            s = next_s
+            learner_loss += (a - a_sup)**2
+            t += 1
+
+            if done == True:
+                break
+
+        learner_loss /= float(t)
+        learned_dist_losses.append(learner_loss)
+
+        t = 0
+        while t < T:
+            a = agent.sample_action(s)
+            a_sup = sup.intended_action(s)
+            a_dist_gen = dist_gen.sample_action(s)
+            # Evaluate losses on learner's distribution
+            next_s, r, done, _ = env.step(a_dist_gen) 
+            s = next_s
+            dist_gen_loss += (a - a_sup)**2
+            t += 1
+
+            if done == True:
+                break
+
+        dist_gen_loss /= float(t)
+        dist_gen_param_losses.append(dist_gen_loss)
+
+            
+    return np.array(learned_dist_losses) - np.array(dist_gen_param_losses)
 
 
 def eval_sup_statistics_cont(env, agent, sup, T, num_samples=1):